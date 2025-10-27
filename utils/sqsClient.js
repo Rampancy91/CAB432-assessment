d0ed867697490c3c94e8305d358731cacbce4325
Nathan <nathd.turner@gmail.com>
2025-10-27 13:18:11 +1000

    sqsClient fix

--- utils/sqsClient.js
+++ utils/sqsClient.js
@@ -1,21 +1,25 @@
 const { SQSClient, SendMessageCommand, ReceiveMessageCommand, DeleteMessageCommand } = require('@aws-sdk/client-sqs');
 
 const AWS_REGION = process.env.AWS_REGION || 'ap-southeast-2';
-let QUEUE_URL = process.env.QUEUE_URL;
 
 // Create SQS client with explicit region
 const sqsClient = new SQSClient({
     region: AWS_REGION,
 });
 
-// Send a message to the queue
-async function sendMessage(messageBody) {
-    if (!QUEUE_URL) {
+// Get queue URL dynamically (don't cache it at module load time)
+function getQueueUrl() {
+    const queueUrl = process.env.QUEUE_URL;
+    if (!queueUrl) {
         throw new Error('QUEUE_URL environment variable is not set');
     }
-    
+    return queueUrl;
+}
+
+// Send a message to the queue
+async function sendMessage(messageBody) {
     const command = new SendMessageCommand({
-        QueueUrl: QUEUE_URL,
+        QueueUrl: getQueueUrl(),
         MessageBody: JSON.stringify(messageBody)
     });
     
@@ -24,12 +28,8 @@ async function sendMessage(messageBody) {
 
 // Receive messages from the queue
 async function receiveMessages(maxMessages = 1, waitTimeSeconds = 20) {
-    if (!QUEUE_URL) {
-        throw new Error('QUEUE_URL environment variable is not set');
-    }
-    
     const command = new ReceiveMessageCommand({
-        QueueUrl: QUEUE_URL,
+        QueueUrl: getQueueUrl(),
         MaxNumberOfMessages: maxMessages,
         WaitTimeSeconds: waitTimeSeconds,
         VisibilityTimeout: 900
@@ -41,27 +41,17 @@ async function receiveMessages(maxMessages = 1, waitTimeSeconds = 20) {
 
 // Delete a message after processing
 async function deleteMessage(receiptHandle) {
-    if (!QUEUE_URL) {
-        throw new Error('QUEUE_URL environment variable is not set');
-    }
-    
     const command = new DeleteMessageCommand({
-        QueueUrl: QUEUE_URL,
+        QueueUrl: getQueueUrl(),
         ReceiptHandle: receiptHandle
     });
     
     return await sqsClient.send(command);
 }
 
-// Allow updating the queue URL after initialization
-function setQueueUrl(url) {
-    QUEUE_URL = url;
-}
-
 module.exports = {
     sendMessage,
     receiveMessages,
     deleteMessage,
-    getQueueUrl: () => QUEUE_URL,
-    setQueueUrl
+    getQueueUrl
 };
